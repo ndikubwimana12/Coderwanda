6f55c5b9282479e01db456f989bf7e954f077819
Eric12 <ndikubwimanaeric2019@gmail.com>
2026-09-25 14:42:19 +0200

    Configure Vercel Express API routing

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,3 @@
 const app = require('../app');
 
-module.exports = (req, res) => {
-    if (!req.url.startsWith('/api')) {
-        req.url = '/api' + (req.url || '/');
-    }
-
-    return app(req, res);
-};
+module.exports = app;
