a872067181287ef601714bd196b696af99f2db8d
Eric12 <ndikubwimanaeric2019@gmail.com>
2026-09-25 14:05:35 +0200

    Fix Vercel API routing

--- server/api/index.js
+++ server/api/index.js
@@ -1,1 +1,9 @@
-module.exports = require('../app');
+const app = require('../app');
+
+module.exports = (req, res) => {
+    if (!req.url.startsWith('/api')) {
+        req.url = '/api' + (req.url || '/');
+    }
+
+    return app(req, res);
+};
