1f68534ef185130a80299b6c584365cd30de9a52
Eric12 <ndikubwimanaeric2019@gmail.com>
2026-09-25 13:31:38 +0200

    Configure Aiven MySQL connection

--- server/db.js
+++ server/db.js
@@ -6,6 +6,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME || "coderwanda",
+
+    ssl: process.env.DB_SSL === "true"
+        ? { rejectUnauthorized: true }
+        : undefined,
+
     waitForConnections: true,
     connectionLimit: 10,
 });
